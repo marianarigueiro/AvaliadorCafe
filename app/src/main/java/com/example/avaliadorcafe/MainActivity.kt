197d71ac32e8e761cafbc2ff8ca58fe107f8b965
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2026-05-26 20:06:52 -0300

    Corrigido erro do compilador

--- app/src/main/java/com/example/avaliadorcafe/MainActivity.kt
+++ app/src/main/java/com/example/avaliadorcafe/MainActivity.kt
@@ -1,13 +1,48 @@
 package com.example.avaliadorcafe
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.avaliadorcafe.adapter.CafeAdapter
+import com.example.avaliadorcafe.data.AppDatabase
+import com.example.avaliadorcafe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.recyclerCafe.layoutManager =
+            LinearLayoutManager(this)
+
+        binding.btnAdicionar.setOnClickListener {
+
+            startActivity(
+                Intent(this, CadastroActivity::class.java)
+            )
+        }
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+
+        val db = AppDatabase.getDatabase(this)
+
+        val lista = db.cafeDao().listar()
+
+        val adapter = CafeAdapter(lista) {
+
+        }
+
+        binding.recyclerCafe.adapter = adapter
     }
 }
